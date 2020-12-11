7fdc1d1b32634248f52c27a3637d79bfd367c9a3
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2020-12-11 08:39:58 +0200

    Add files via upload

---
+++ PaintApplication/ChangePen.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PaintApplication
+{
+    class ChangePen
+    {
+        public void ChangePenWeight(Pen pen, int width)
+        {
+            pen.Width = width;
+        }
+
+        public void ChangePenColor(ColorDialog colorDialog, Pen pen, ToolStripButton btn)
+        {
+            colorDialog.ShowDialog();
+            Color color = colorDialog.Color;
+            pen.Color = color;
+            btn.BackColor = color;
+        }
+
+    }
+}
